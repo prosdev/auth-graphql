eb714345ccc98e7ae541a9ff0668ce67c485d1ca
prosdev <prosdevlab@gmail.com>
2017-06-11 06:27:44 -0700

    Check auth status

--- server/schema/rootQueryType.js
+++ server/schema/rootQueryType.js
@@ -1,14 +1,19 @@
 const graphql = require('graphql');
 const {
-  GraphQLObjectType,
-  GraphQLID
+    GraphQLObjectType,
 } = graphql;
+const UserType = require('./userType');
 
 const RootQueryType = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    stubField: { type: GraphQLID }
-  }
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                return req.user;
+            }
+        }
+    }
 });
 
 module.exports = RootQueryType;
